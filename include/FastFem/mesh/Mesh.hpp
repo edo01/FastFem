6df570e27efd7e5e170fb0285eeabca11d47ebd9
edo01 <edocarra012@gmail.com>
2025-02-18 12:36:02 +0100

    Split definition and declaration of Mesh

--- include/FastFem/mesh/Mesh.hpp
+++ include/FastFem/mesh/Mesh.hpp
@@ -7,6 +7,13 @@
 #ifndef MESH_HPP
 #define MESH_HPP
 
+#include <stdint.h>
+
+#include "FastFem/mesh/Geometry.hpp"
+#include "FastFem/mesh/VertexHasher.hpp"
+#include "FastFem/common/hash_table.h"
+#include "FastFem/types/CommonTypes.hpp"
+
 constexpr inline size_t binom(size_t n, size_t k) noexcept
 {
     return
@@ -20,13 +27,6 @@ constexpr inline size_t binom(size_t n, size_t k) noexcept
 
 #define get_m_faces_on_n_simplex(m, n) (binom(n+1, m+1))
 
-#include <stdint.h>
-
-#include "FastFem/mesh/Geometry.hpp"
-#include "FastFem/mesh/VertexHasher.hpp"
-#include "FastFem/common/hash_table.h"
-#include "FastFem/types/CommonTypes.hpp"
-
 namespace fastfem {
 namespace mesh {
 
@@ -36,15 +36,7 @@ struct Vertex
 {
     Point<spacedim> point;
 
-    bool operator==(const Vertex<spacedim> &v) const 
-    {
-        for (unsigned int i = 0; i < spacedim; ++i)
-        {
-            if (point.coords[i] != v.point.coords[i])
-                return false;
-        }
-        return true;
-    }
+    bool operator==(const Vertex<spacedim> &v) const;
 };
 
 /**
@@ -73,117 +65,36 @@ public:
     static constexpr unsigned int n_faces = get_m_faces_on_n_simplex(2, dim);
     static constexpr unsigned int n_cells = get_m_faces_on_n_simplex(3, dim);
 
-    MeshSimplex(const size_t v[dim + 1])
-    {
-        std::copy(v, v + dim + 1, vertices.begin());
-    }
+    MeshSimplex(const size_t v[dim + 1]);
 
     /**
      * Get the indices of the vertices of the simplex. 
      */
-    const std::array<fastfem::types::simplex_index<0>, n_vertices> get_vertex_indices() const {
-        std::array<fastfem::types::simplex_index<0>, n_vertices> v_indices;
-
-        unsigned int vertex_count = 0;
-
-        for (size_t i = 0; i < dim + 1; ++i)
-        {
-            std::array<size_t, 1> v = {vertices[i]};
-            v_indices[vertex_count++] = v;
-        }
-        return v_indices;
-    }
+    const std::array<fastfem::types::simplex_index<0>, n_vertices> get_vertex_indices() const;
 
     /**
      * Get the indices of the edges of the simplex. Each edge is represented by an
      * ordered pair of indices of the vertices.
      */
-    const std::array<fastfem::types::simplex_index<1>, n_edges> get_edges_indices() const {
-        std::array<fastfem::types::simplex_index<1>, n_edges> e_indices;
-        unsigned int edge_count = 0;
-
-        for (size_t i = 0; i < dim + 1; ++i)
-        {
-            for (size_t j = i + 1; j < dim + 1; ++j)
-            {
-                // always store the indices in ascending order
-                if(vertices[i] < vertices[j])
-                    e_indices[edge_count++] = {vertices[i], vertices[j]};
-                else
-                    e_indices[edge_count++] = {vertices[j], vertices[i]};
-            }
-        }
-        
-        assert(edge_count == n_edges);
-
-        return e_indices;
-    }    
+    const std::array<fastfem::types::simplex_index<1>, n_edges> get_edges_indices() const;
 
     /**
-     * Get the indices of the faces of the simplex. Each face is represented by an
+     * Get the indices of the faces of the simplex, if present. Each face is represented by an
      * ordered triple of indices of the vertices. 
      */
-    const std::array<fastfem::types::simplex_index<2>, n_faces> get_faces_indices() const {
-        if(n_faces == 0) return {}; // in 1D there are no faces
+    const std::array<fastfem::types::simplex_index<2>, n_faces> get_faces_indices() const; 
 
-        std::array<fastfem::types::simplex_index<2>, n_faces> f_indices;
+    /**
+     * Get the indices of the cells of the simplex, if present. Each cell is represented by an
+     * ordered quadruple of indices of the vertices.
+     */
+    const std::array<fastfem::types::simplex_index<3>, n_cells> get_cell_indices() const;
 
-        unsigned int face_count = 0;
+    bool operator==(const MeshSimplex<dim, spacedim> &s) const;
 
-        for (size_t i = 0; i < dim + 1; ++i){
-            for (size_t j = i + 1; j < dim + 1; ++j){
-                for (size_t k = j + 1; k < dim + 1; ++k){
-                    fastfem::types::simplex_index<2> face = {vertices[i], vertices[j], vertices[k]};
-                    std::sort(face.begin(), face.end());
-                    // copy starting from the face_count index
-                    f_indices[face_count++] = face;
-                }
-            }
-        }
-
-        assert(face_count == n_faces);
-
-        return f_indices;
-    }
-
-    const std::array<fastfem::types::simplex_index<3>, n_cells> get_cell_indices() const {
-        if(n_cells == 0) return {}; // in 1D and 2D there are no cells
-        
-        std::array<fastfem::types::simplex_index<3>, n_cells> c_indices;
-
-        unsigned int cell_count = 0;
-
-        for (size_t i = 0; i < dim + 1; ++i){
-            for (size_t j = i + 1; j < dim + 1; ++j){
-                for (size_t k = j + 1; k < dim + 1; ++k){
-                    for (size_t l = k + 1; l < dim + 1; ++l){
-                        fastfem::types::simplex_index<3> cell = {vertices[i], vertices[j], vertices[k], vertices[l]};
-                        std::sort(cell.begin(), cell.end());
-                        c_indices[cell_count++] = cell;                        
-                    }
-                }
-            }
-        }
-
-        assert(cell_count == n_cells);
-
-        return c_indices;
-    }
-
-    // just check if the vertices are the same
-    bool operator==(const MeshSimplex<dim, spacedim> &s) const
-    {
-        for (unsigned int i = 0; i < dim + 1; ++i)
-        {
-            if (vertices[i] != s.vertices[i])
-                return false;
-        }
-        return true;
-    }
-
-    void set_vertex(size_t i, size_t v) { vertices[i] = v; }
-    size_t get_vertex(size_t i) const { return vertices[i]; }
-    int vertex_count() const { return dim + 1; }
+    inline void set_vertex(size_t i, size_t v) { vertices[i] = v; }
+    inline size_t get_vertex(size_t i) const { return vertices[i]; }
+    inline int vertex_count() const { return dim + 1; }
 
 private:
     std::array<size_t, dim + 1> vertices;
@@ -200,47 +111,37 @@ class Mesh
 {
 
 public:
-    size_t vtx_count() const { return vertices.size(); }
-    size_t elem_count() const { return elements.size(); }
+    inline size_t vtx_count() const { return vertices.size(); }
+    inline size_t elem_count() const { return elements.size(); }
 
-    void add_vertex(const Vertex<spacedim> &v) { vertices.push_back(v); }
-    void add_element(const MeshSimplex<dim, spacedim> &e) { elements.push_back(e); }
+    inline void add_vertex(const Vertex<spacedim> &v) { vertices.push_back(v); }
+    inline void add_element(const MeshSimplex<dim, spacedim> &e) { elements.push_back(e); }
 
-    void set_vertex(size_t i, const Vertex<spacedim> &v) { vertices[i] = v; }
-    void set_element(size_t i, const MeshSimplex<dim, spacedim> &e) { elements[i] = e; }
+    inline void set_vertex(size_t i, const Vertex<spacedim> &v) { vertices[i] = v; }
+    inline void set_element(size_t i, const MeshSimplex<dim, spacedim> &e) { elements[i] = e; }
 
-    Vertex<spacedim> &get_vertex(size_t i) { return vertices[i]; }
-    MeshSimplex<dim, spacedim> &get_mesh_element(size_t i) { return elements[i]; }
+    inline Vertex<spacedim> &get_vertex(size_t i) { return vertices[i]; }
+    inline MeshSimplex<dim, spacedim> &get_mesh_element(size_t i) { return elements[i]; }
 
-    void resize_vertices(size_t n) { vertices.resize(n); }
+    inline void resize_vertices(size_t n) { vertices.resize(n); }
 
-    void reserve_vertices(size_t n) { vertices.reserve(n); }
-    void reserve_elements(size_t n) { elements.reserve(n); }
+    inline void reserve_vertices(size_t n) { vertices.reserve(n); }
+    inline void reserve_elements(size_t n) { elements.reserve(n); }
 
-    auto vtx_begin() { return vertices.begin(); }
-    auto vtx_end() { return vertices.end(); }
+    inline auto vtx_begin() { return vertices.begin(); }
+    inline auto vtx_end() { return vertices.end(); }
 
-    auto elem_begin() { return elements.begin(); }
-    auto elem_end() { return elements.end(); }
+    inline auto elem_begin() { return elements.begin(); }
+    inline auto elem_end() { return elements.end(); }
 
-    auto elem_begin() const { return elements.begin(); }
-    auto elem_end() const { return elements.end(); }
+    inline auto elem_begin() const { return elements.begin(); }
+    inline auto elem_end() const { return elements.end(); }
 
-    void add_boundary(size_t i) { boundary.push_back(i); }
-    size_t boundary_count() const { return boundary.size(); }
-    Vertex<spacedim> get_boundary_vertex(size_t i) const { return vertices[boundary[i]]; }
+    inline void add_boundary(size_t i) { boundary.push_back(i); }
+    inline size_t boundary_count() const { return boundary.size(); }
+    inline Vertex<spacedim> get_boundary_vertex(size_t i) const { return vertices[boundary[i]]; }
     
-    Simplex<dim, spacedim> get_Simplex(MeshSimplex<dim, spacedim> s) const
-    {
-        Point<spacedim> p[dim + 1];
-        for (size_t i = 0; i < dim + 1; ++i)
-        {
-            p[i] = vertices[s.get_vertex(i)];
-        }
-        return Simplex<dim, spacedim>(p);
-    }
-
- 
+    Simplex<dim, spacedim> get_Simplex(MeshSimplex<dim, spacedim> s) const;
 
     private:
     std::vector<Vertex<spacedim>> vertices;
---
+++ src/mesh/Mesh.cpp
@@ -0,0 +1,164 @@
+#include "FastFem/mesh/Mesh.hpp"
+
+namespace fastfem{
+namespace mesh{
+
+/*
+ * ----------------------VECTOR----------------------------
+ */
+template<unsigned int spacedim>
+bool Vertex<spacedim>::operator==(const Vertex<spacedim> &v) const 
+{
+    for (unsigned int i = 0; i < spacedim; ++i)
+    {
+        if (point.coords[i] != v.point.coords[i])
+            return false;
+    }
+    return true;
+}
+
+/*
+ * --------------------MESH SIMPLEX-------------------------
+ */
+
+template<unsigned int dim, unsigned int spacedim>
+MeshSimplex<dim, spacedim>::MeshSimplex(const size_t v[dim + 1])     
+{
+    std::copy(v, v + dim + 1, vertices.begin());
+}
+
+template<unsigned int dim, unsigned int spacedim>
+const std::array<fastfem::types::simplex_index<0>, MeshSimplex<dim, spacedim>::n_vertices> 
+MeshSimplex<dim, spacedim>::get_vertex_indices() const {
+
+    std::array<fastfem::types::simplex_index<0>, n_vertices> v_indices;
+    unsigned int vertex_count = 0;
+
+    for (size_t i = 0; i < dim + 1; ++i)
+    {
+        std::array<size_t, 1> v = {vertices[i]};
+        v_indices[vertex_count++] = v;
+    }
+    return v_indices;
+}
+
+template<unsigned int dim, unsigned int spacedim>
+const std::array<fastfem::types::simplex_index<1>, MeshSimplex<dim, spacedim>::n_edges> 
+MeshSimplex<dim, spacedim>::get_edges_indices() const {
+    std::array<fastfem::types::simplex_index<1>, n_edges> e_indices;
+    unsigned int edge_count = 0;
+
+    for (size_t i = 0; i < dim + 1; ++i)
+    {
+        for (size_t j = i + 1; j < dim + 1; ++j)
+        {
+            // always store the indices in ascending order
+            if(vertices[i] < vertices[j])
+                e_indices[edge_count++] = {vertices[i], vertices[j]};
+            else
+                e_indices[edge_count++] = {vertices[j], vertices[i]};
+        }
+    }
+    
+    assert(edge_count == n_edges);
+
+    return e_indices;
+}
+
+template<unsigned int dim, unsigned int spacedim>
+const std::array<fastfem::types::simplex_index<2>, MeshSimplex<dim, spacedim>::n_faces> 
+MeshSimplex<dim, spacedim>::get_faces_indices() const {
+    if(n_faces == 0) return {}; // in 1D there are no faces
+
+    std::array<fastfem::types::simplex_index<2>, n_faces> f_indices;
+
+    unsigned int face_count = 0;
+
+    for (size_t i = 0; i < dim + 1; ++i){
+        for (size_t j = i + 1; j < dim + 1; ++j){
+            for (size_t k = j + 1; k < dim + 1; ++k){
+                fastfem::types::simplex_index<2> face = {vertices[i], vertices[j], vertices[k]};
+                std::sort(face.begin(), face.end());
+                // copy starting from the face_count index
+                f_indices[face_count++] = face;
+            }
+        }
+    }
+
+    assert(face_count == n_faces);
+
+    return f_indices;
+}
+
+template<unsigned int dim, unsigned int spacedim>
+const std::array<fastfem::types::simplex_index<3>, MeshSimplex<dim, spacedim>::n_cells> 
+MeshSimplex<dim, spacedim>::get_cell_indices() const {
+    if(n_cells == 0) return {}; // in 1D and 2D there are no cells
+    
+    std::array<fastfem::types::simplex_index<3>, n_cells> c_indices;
+
+    unsigned int cell_count = 0;
+
+    for (size_t i = 0; i < dim + 1; ++i){
+        for (size_t j = i + 1; j < dim + 1; ++j){
+            for (size_t k = j + 1; k < dim + 1; ++k){
+                for (size_t l = k + 1; l < dim + 1; ++l){
+                    fastfem::types::simplex_index<3> cell = {vertices[i], vertices[j], vertices[k], vertices[l]};
+                    std::sort(cell.begin(), cell.end());
+                    c_indices[cell_count++] = cell;                        
+                }
+            }
+        }
+    }
+
+    assert(cell_count == n_cells);
+
+    return c_indices;
+}
+
+template<unsigned int dim, unsigned int spacedim>
+bool MeshSimplex<dim, spacedim>::operator==(const MeshSimplex<dim, spacedim> &s) const
+{
+    for (unsigned int i = 0; i < dim + 1; ++i)
+    {
+        if (vertices[i] != s.vertices[i])
+            return false;
+    }
+    return true;
+}
+
+
+/*
+ * ----------------------MESH------------------------------
+ */
+template<unsigned int dim, unsigned int spacedim>
+Simplex<dim, spacedim> Mesh<dim, spacedim>::get_Simplex(MeshSimplex<dim, spacedim> s) const
+{
+    Point<spacedim> p[dim + 1];
+    for (size_t i = 0; i < dim + 1; ++i)
+    {
+        p[i] = vertices[s.get_vertex(i)];
+    }
+    return Simplex<dim, spacedim>(p);
+}
+
+
+/**
+ * ---------------Template instantiations-------------------
+ */
+
+// vertex
+template struct Vertex<1>;
+template struct Vertex<2>;
+template struct Vertex<3>;
+
+// mesh simplex
+template class MeshSimplex<1, 1>;
+template class MeshSimplex<1, 2>;
+template class MeshSimplex<1, 3>;
+template class MeshSimplex<2, 2>;
+template class MeshSimplex<2, 3>;
+template class MeshSimplex<3, 3>;
+
+} // namespace mesh
+} // namespace fastfem
