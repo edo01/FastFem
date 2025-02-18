d04538feab28fb9e297fef259e451b0ed705a029
edo01 <edocarra012@gmail.com>
2025-02-18 12:36:16 +0100

    Split definition and declaration of Mesh

--- include/FastFem/mesh/Geometry.hpp
+++ include/FastFem/mesh/Geometry.hpp
@@ -11,20 +11,31 @@ template <unsigned int spacedim>
 struct Point
 {
     double coords[spacedim];
+
+    bool operator==(const Point<spacedim> &p) const;
+
+    inline bool operator!=(const Point<spacedim> &p) const { return !(*this == p); }
     
 };
 
 template <unsigned int dim, unsigned int spacedim=dim>
 class Simplex
 {
-public:
+    /*
+     * Some static assertions to make sure that the template arguments are correct
+     * based on the following constraints:
+     *  - we don't want to have a simplex with a dimension greater than the space it lives in
+     *  - the dimension of the simplex must be greater than 0
+     *  - we don't handle spaces with dimension greater than 3
+     */
+    static_assert(dim >= 0, "The dimension of the simplex must be equal or greater than 0");
+    static_assert(dim <= spacedim, "The dimension of the simplex must be less or equal to the space it lives in");
+    static_assert(spacedim <= 3, "The space dimension must be less or equal to 3");
 
+public:
     Simplex() = default;
     
-    Simplex(const Point<spacedim> v[dim + 1])
-    {
-        std::copy(v, v + dim + 1, vertices);
-    }
+    Simplex(const Point<spacedim> v[dim + 1]);
 
     template <typename... Args>
     Simplex(Args... args)
@@ -33,75 +44,29 @@ public:
         Point<spacedim> temp[] = {args...};
         std::copy(temp, temp + dim + 1, vertices);
     }
-
-    // compute the volume of the simplex
-    double volume() const
-    {
-        if(dim==0) return 0;
-        if(dim==1) return vertices[1].coords[0] - vertices[0].coords[0];
-        if(dim==2) return 0.5 * ((vertices[1].coords[0] - vertices[0].coords[0]) * (vertices[2].coords[1] - vertices[0].coords[1]) - (vertices[2].coords[0] - vertices[0].coords[0]) * (vertices[1].coords[1] - vertices[0].coords[1]));
-        else return 0; // not implemented
-    }
     
-    Point<spacedim> get_centroid() const
-    {
-        Point<spacedim> centroid;
-        for (size_t i = 0; i < dim + 1; ++i)
-        {
-            for (size_t j = 0; j < spacedim; ++j)
-            {
-                centroid.coords[j] += vertices[i].coords[j];
-            }
-        }
-        for (size_t j = 0; j < spacedim; ++j)
-        {
-            centroid.coords[j] /= dim + 1;
-        }
-        return centroid;
-    }
 
-    std::vector<Point<spacedim>> get_vertices() const
-    {
-        return std::vector<Point<spacedim>>(vertices, vertices + dim + 1);
-    }
-
-    Point<spacedim> get_vertex(size_t i) const
-    {
-        return vertices[i];
-    }
-
-    // An intersection between two simplices is a simplex of lower dimension.
-    // Since a simplex is completely defined by its vertices, we can simply return the vertices that are common 
-    // to the two simplices.
-    std::vector<Point<spacedim>> intersection(const Simplex<dim> &s) const
-    {
-        std::vector<Point<spacedim>> result;
-        for (size_t i = 0; i < dim + 1; ++i)
-        {
-            for (size_t j = 0; j < dim + 1; ++j)
-            {
-                if (vertices[i] == s.vertices[j])
-                {
-                    result.push_back(vertices[i]);
-                    break;
-                }
-            }
-        }
-        return result;
-    }
+    double volume() const;
     
-    bool operator==(const Simplex<dim> &s) const
-    {
-        for (size_t i = 0; i < dim + 1; ++i)
-        {
-            if (vertices[i] != s.vertices[i])
-                return false;
-        }
-        return true;
-    }
+    /**
+     * Returns the centroid of the simplex.
+     */
+    Point<spacedim> get_centroid() const;
 
-    int n_vertices() const { return num_vertices; }
-    int n_edges() const { return num_edges; }
+    std::vector<Point<spacedim>> get_vertices() const;
+
+    Point<spacedim> get_vertex(size_t i) const;
+
+    /**
+     * Returns the intersection of this simplex with another simplex, which
+     * is identified by its vertices.
+     */
+    std::vector<Point<spacedim>> intersection(const Simplex<dim, spacedim> &s) const;
+    
+    bool operator==(const Simplex<dim, spacedim> &s) const;
+
+    int n_vertices() const;
+    int n_edges() const;
 
 private:
     Point<spacedim> vertices[dim + 1];
---
+++ src/mesh/Geometry.cpp
@@ -0,0 +1,124 @@
+#include "FastFem/mesh/Geometry.hpp"
+
+namespace fastfem{
+namespace mesh{
+
+
+/**
+ * ------------------------ Point class ------------------------
+ */
+
+template <unsigned int spacedim>
+bool Point<spacedim>::operator==(const Point<spacedim> &p) const
+{
+    for (size_t i = 0; i < spacedim; ++i)
+    {
+        if (coords[i] != p.coords[i])
+            return false;
+    }
+    return true;
+}
+
+
+/**
+ * ------------------------ Simplex class ------------------------
+ */
+
+template <unsigned int dim, unsigned int spacedim>
+Simplex<dim, spacedim>::Simplex(const Point<spacedim> v[dim + 1])
+{
+    std::copy(v, v + dim + 1, vertices);
+}
+
+template <unsigned int dim, unsigned int spacedim>
+double Simplex<dim, spacedim>::volume() const
+{
+    if(dim==0) return 0;
+    if(dim==1) return vertices[1].coords[0] - vertices[0].coords[0];
+    if(dim==2) return 0.5 * ((vertices[1].coords[0] - vertices[0].coords[0]) * (vertices[2].coords[1] - vertices[0].coords[1]) - (vertices[2].coords[0] - vertices[0].coords[0]) * (vertices[1].coords[1] - vertices[0].coords[1]));
+    else return 0; // not implemented
+}
+
+template <unsigned int dim, unsigned int spacedim>
+Point<spacedim> Simplex<dim, spacedim>::get_centroid() const
+{
+    Point<spacedim> centroid;
+    for (size_t i = 0; i < dim + 1; ++i)
+    {
+        for (size_t j = 0; j < spacedim; ++j)
+        {
+            centroid.coords[j] += vertices[i].coords[j];
+        }
+    }
+    for (size_t j = 0; j < spacedim; ++j)
+    {
+        centroid.coords[j] /= dim + 1;
+    }
+    return centroid;
+}
+
+template <unsigned int dim, unsigned int spacedim>
+std::vector<Point<spacedim>> Simplex<dim, spacedim>::get_vertices() const
+{
+    return std::vector<Point<spacedim>>(vertices, vertices + dim + 1);
+}
+
+template <unsigned int dim, unsigned int spacedim>
+Point<spacedim> Simplex<dim, spacedim>::get_vertex(size_t i) const
+{
+    return vertices[i];
+}
+
+template <unsigned int dim, unsigned int spacedim>
+std::vector<Point<spacedim>> Simplex<dim, spacedim>::intersection(const Simplex<dim, spacedim> &s) const
+{
+    std::vector<Point<spacedim>> result;
+    for (size_t i = 0; i < dim + 1; ++i)
+    {
+        for (size_t j = 0; j < dim + 1; ++j)
+        {
+            if (vertices[i] == s.get_vertex(j))
+            {
+                result.push_back(vertices[i]);
+                break;
+            }
+        }
+    }
+    return result;
+}
+
+template <unsigned int dim, unsigned int spacedim>
+bool Simplex<dim, spacedim>::operator==(const Simplex<dim, spacedim> &s) const
+{
+    for (size_t i = 0; i < dim + 1; ++i)
+    {
+        if (vertices[i] != s.get_vertex(i))
+            return false;
+    }
+    return true;
+}
+
+template <unsigned int dim, unsigned int spacedim>
+int Simplex<dim, spacedim>::n_vertices() const { return num_vertices; }
+
+template <unsigned int dim, unsigned int spacedim>
+int Simplex<dim, spacedim>::n_edges() const { return num_edges; }
+
+
+// Explicit instantiation
+
+template class Point<1>;
+template class Point<2>;
+template class Point<3>;
+
+template class Simplex<0, 1>;
+template class Simplex<1, 1>;
+template class Simplex<1, 2>;
+template class Simplex<1, 3>;
+template class Simplex<2, 2>;
+template class Simplex<2, 3>;
+template class Simplex<3, 3>;
+
+
+} // namespace mesh
+} // namespace fastfem
